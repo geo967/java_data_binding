bb93c655113e9890fe605c60d2ffce45035f8e9a
geo967 <geomathew967@gmail.com>
2022-01-06 12:12:12 +0530

    c1

---
+++ app/src/main/java/com/example/databinding/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.databinding;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.databinding.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // ViewModel updates the Model
+        // after observing changes in the View
+
+        // model will also update the view
+        // via the ViewModel
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setViewModel(new AppViewModel());
+        activityMainBinding.executePendingBindings();
+
+    }
+
+    
+}
